83a5b650090564331bf9c120bfaa819de46d583b
Cydox <Cydox@users.noreply.github.com>
2016-07-07 02:05:46 +0200

    Update preview.js

--- frontend/js/preview.js
+++ frontend/js/preview.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	var socket = io.connect(location.origin);
 		 socket.on("content", function(data){
+				 var tempScrollTop = $(window).scrollTop();
+
 				 console.log("got data");
 
 				 $("body").html(data.html);
-				 MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+				 MathJax.Hub.Queue(["Typeset",MathJax.Hub], function() {
+					 $(window).scrollTop(tempScrollTop);
+				 });
 		 });
 })
